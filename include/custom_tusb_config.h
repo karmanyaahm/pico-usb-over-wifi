d01444d812e4668c703270f8f18efb405102a9ff
Karmanyaah Malhotra <karmanyaah.git@malhotra.cc>
2022-08-14 18:29:02 -0500

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

--- include/custom_tusb_config.h
+++ include/custom_tusb_config.h
@@ -1,69 +1,192 @@
-/*
-    The MIT License (MIT)
-    Copyright (c) 2018, hathach for Adafruit
-*/
-
-#define _TUSB_CONFIG_RP2040_H_
+#ifndef _TUSB_CONFIG_H_
+#define _TUSB_CONFIG_H_
 
 #ifdef __cplusplus
-extern "C" {
+ extern "C" {
+#endif
+
+//--------------------------------------------------------------------+
+// Board Specific Configuration
+//--------------------------------------------------------------------+
+
+// RHPort number used for host can be defined by board.mk, default to port 0
+#ifndef BOARD_TUH_RHPORT
+#define BOARD_TUH_RHPORT      1
+#endif
+
+// RHPort max operational speed can defined by board.mk
+#ifndef BOARD_TUH_MAX_SPEED
+#define BOARD_TUH_MAX_SPEED   OPT_MODE_DEFAULT_SPEED
 #endif
 
 //--------------------------------------------------------------------
 // COMMON CONFIGURATION
 //--------------------------------------------------------------------
 
-// Enable device stack
-#define CFG_TUD_ENABLED 0
-
+// defined by compiler flags for flexibility
 #ifndef CFG_TUSB_MCU
-#define CFG_TUSB_MCU OPT_MCU_RP2040
+#error CFG_TUSB_MCU must be defined
 #endif
-#define CFG_TUSB_OS OPT_OS_PICO
+
+#ifndef CFG_TUSB_OS
+#define CFG_TUSB_OS           OPT_OS_NONE
+#endif
+
+//#define PICO_CYW43_ARCH_THREADSAFE_BACKGROUND
 
 #ifndef CFG_TUSB_DEBUG
-#define CFG_TUSB_DEBUG 0
+#define CFG_TUSB_DEBUG 2
+#define DEBUG_RP2040_PORT Serial1
 #endif
 
 #if CFG_TUSB_DEBUG
 #define CFG_TUSB_DEBUG_PRINTF serial1_printf
+#define CYW43_ARCH_DEBUG serial1_printf
 extern int serial1_printf(const char *__restrict __format, ...);
 #endif
+// Enable Host stack
+#define CFG_TUH_ENABLED       1
 
+// Default is max speed that hardware controller could support with on-chip PHY
+#define CFG_TUH_MAX_SPEED     BOARD_TUH_MAX_SPEED
+
+/* USB DMA on some MCUs can only access a specific SRAM region with restriction on alignment.
+ * Tinyusb use follows macros to declare transferring memory so that they can be put
+ * into those specific section.
+ * e.g
+ * - CFG_TUSB_MEM SECTION : __attribute__ (( section(".usb_ram") ))
+ * - CFG_TUSB_MEM_ALIGN   : __attribute__ ((aligned(4)))
+ */
+#ifndef CFG_TUSB_MEM_SECTION
 #define CFG_TUSB_MEM_SECTION
-#define CFG_TUSB_MEM_ALIGN TU_ATTR_ALIGNED(4)
+#endif
 
-// Device Configuration
-
-// Don't want this, but need this to prevent SerialUSB errors
-//#define CFG_TUSB_RHPORT1_MODE OPT_MODE_DEVICE
-//#define CFG_TUD_CDC 1
-//
-//#define CFG_TUD_CDC_RX_BUFSIZE 256
-//#define CFG_TUD_CDC_TX_BUFSIZE 256
+#ifndef CFG_TUSB_MEM_ALIGN
+#define CFG_TUSB_MEM_ALIGN          __attribute__ ((aligned(4)))
+#endif
 
 //--------------------------------------------------------------------
-// Host Configuration
+// CONFIGURATION
 //--------------------------------------------------------------------
+#define CFG_TUH_RPI_PIO_USB 1
 
-#define CFG_TUH_ENABLE 1
-
-#define CFG_TUSB_RHPORT0_MODE OPT_MODE_HOST | OPT_MODE_LOW_SPEED
-
-#define CFG_TUH_API_EDPT_XFER       1
+// Size of buffer to hold descriptors and other data used for enumeration
 #define CFG_TUH_ENUMERATION_BUFSIZE 256
 
-
-#define CFG_TUH_HUB                 1
+// only hub class is enabled
+#define CFG_TUH_HUB                 0
 #define CFG_TUH_CDC                 2
-#define CFG_TUH_HID                 0
-#define CFG_TUH_MSC                 0
-#define CFG_TUH_VENDOR              0
 
 // max device support (excluding hub device)
-#define CFG_TUH_DEVICE_MAX          (CFG_TUH_HUB ? 4 : 1) // hub typically has 4 ports
+// 1 hub typically has 4 ports
+#define CFG_TUH_DEVICE_MAX          (CFG_TUH_HUB ? 4 : 2)
+
+// Max endpoint per device
+#define CFG_TUH_ENDPOINT_MAX        8
+
+// Enable tuh_edpt_xfer() API
+#define CFG_TUH_API_EDPT_XFER       1
+
+
+#define CFG_TUD_CDC_RX_BUFSIZE   512
+#define CFG_TUD_CDC_TX_BUFSIZE   512
+#define CFG_TUD_CDC_EP_BUFSIZE   512
 
 
 #ifdef __cplusplus
-}
+ }
 #endif
+
+#endif /* _TUSB_CONFIG_H_ */
+
+
+// Common settings used in most of the pico_w examples
+// (see https://www.nongnu.org/lwip/2_1_x/group__lwip__opts.html for details)
+
+// allow override in some examples
+#ifndef NO_SYS
+#define NO_SYS                      1
+#endif
+// allow override in some examples
+#ifndef LWIP_SOCKET
+#define LWIP_SOCKET                 0
+#endif
+#if PICO_CYW43_ARCH_POLL
+#define MEM_LIBC_MALLOC             1
+#else
+
+#define CYW43_NETUTIL 1
+#define PICO_CYW43_ARCH_THREADSAFE_BACKGROUND 0
+#define PICO_CYW43_ARCH_POLL 1
+
+
+// MEM_LIBC_MALLOC is incompatible with non polling versions
+#define MEM_LIBC_MALLOC             0
+#endif
+#define MEM_ALIGNMENT               4
+#define MEM_SIZE                    4000
+#define MEMP_NUM_TCP_SEG            32
+#define MEMP_NUM_ARP_QUEUE          10
+#define PBUF_POOL_SIZE              24
+#define LWIP_ARP                    1
+#define LWIP_ETHERNET               1
+#define LWIP_ICMP                   1
+#define LWIP_RAW                    1
+#define TCP_WND                     (8 * TCP_MSS)
+#define TCP_MSS                     1460
+#define TCP_SND_BUF                 (8 * TCP_MSS)
+#define TCP_SND_QUEUELEN            ((4 * (TCP_SND_BUF) + (TCP_MSS - 1)) / (TCP_MSS))
+#define LWIP_NETIF_STATUS_CALLBACK  1
+#define LWIP_NETIF_LINK_CALLBACK    1
+#define LWIP_NETIF_HOSTNAME         1
+#define LWIP_NETCONN                0
+#define MEM_STATS                   0
+#define SYS_STATS                   0
+#define MEMP_STATS                  0
+#define LINK_STATS                  0
+// #define ETH_PAD_SIZE                2
+#define LWIP_CHKSUM_ALGORITHM       3
+#define LWIP_DHCP                   1
+#define LWIP_IPV4                   1
+#define LWIP_TCP                    1
+#define LWIP_UDP                    1
+#define LWIP_DNS                    1
+#define LWIP_TCP_KEEPALIVE          1
+#define LWIP_NETIF_TX_SINGLE_PBUF   1
+#define DHCP_DOES_ARP_CHECK         0
+#define LWIP_DHCP_DOES_ACD_CHECK    0
+
+#ifndef NDEBUG
+#define LWIP_DEBUG                  1
+#define LWIP_STATS                  1
+#define LWIP_STATS_DISPLAY          1
+#endif
+
+#define ETHARP_DEBUG                LWIP_DBG_OFF
+#define NETIF_DEBUG                 LWIP_DBG_OFF
+#define PBUF_DEBUG                  LWIP_DBG_OFF
+#define API_LIB_DEBUG               LWIP_DBG_OFF
+#define API_MSG_DEBUG               LWIP_DBG_OFF
+#define SOCKETS_DEBUG               LWIP_DBG_OFF
+#define ICMP_DEBUG                  LWIP_DBG_OFF
+#define INET_DEBUG                  LWIP_DBG_OFF
+#define IP_DEBUG                    LWIP_DBG_OFF
+#define IP_REASS_DEBUG              LWIP_DBG_OFF
+#define RAW_DEBUG                   LWIP_DBG_OFF
+#define MEM_DEBUG                   LWIP_DBG_OFF
+#define MEMP_DEBUG                  LWIP_DBG_OFF
+#define SYS_DEBUG                   LWIP_DBG_OFF
+#define TCP_DEBUG                   LWIP_DBG_OFF
+#define TCP_INPUT_DEBUG             LWIP_DBG_OFF
+#define TCP_OUTPUT_DEBUG            LWIP_DBG_OFF
+#define TCP_RTO_DEBUG               LWIP_DBG_OFF
+#define TCP_CWND_DEBUG              LWIP_DBG_OFF
+#define TCP_WND_DEBUG               LWIP_DBG_OFF
+#define TCP_FR_DEBUG                LWIP_DBG_OFF
+#define TCP_QLEN_DEBUG              LWIP_DBG_OFF
+#define TCP_RST_DEBUG               LWIP_DBG_OFF
+#define UDP_DEBUG                   LWIP_DBG_OFF
+#define TCPIP_DEBUG                 LWIP_DBG_OFF
+#define PPP_DEBUG                   LWIP_DBG_OFF
+#define SLIP_DEBUG                  LWIP_DBG_OFF
+#define DHCP_DEBUG                  LWIP_DBG_OFF
--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,195 @@
-#include <Arduino.h>
-#include <WiFi.h>
+#include "custom_tusb_config.h"
+//#include <SerialUART.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "../lib/tinyusb/hw/bsp/board.h"
 #include "tusb.h"
+#include "pio_usb.h"
+#include "pico/stdlib.h"
+#include "pico/multicore.h"
+#include "pico/bootrom.h"
 
-#define SERVER_HOST "karmanyaahsarch.homenet.malhotra.cc"
+#include "cyw43_ll.h"
 
-auto w = WiFiClient();
-int status = WL_IDLE_STATUS; // the Wifi radio's status
 
-void setup()
+
+#include "lwip/pbuf.h"
+#include "lwip/tcp.h"
+//#include <WiFi.h>
+//#include <WiFiClient.h>
+
+#include "pico/cyw43_arch.h"
+
+// English
+#define LANGUAGE_ID 0x0409
+#define BUF_COUNT 4
+
+tusb_desc_device_t desc_device;
+#define HOST "karmanyaahsarch.homenet.malhotra.cc"
+//WiFiClient client = WiFiClient();
+
+//--------------------------------------------------------------------+
+// MACRO CONSTANT TYPEDEF PROTYPES
+//--------------------------------------------------------------------+
+void led_blinking_task(void);
+
+extern void cdc_task(void);
+
+CFG_TUSB_MEM_SECTION static char serial_in_buffer[512] = {0};
+// 128 long just seems to print nothing at all TODO find out wtf is wrong
+
+void tuh_cdc_xfer_cb(tuh_xfer_t *xfer)
 {
-  Serial1.begin(115200);
-  delay(1);
-  Serial1.println("hiiiiii");
-  WiFi.setHostname("robo-pico");
-
-  Serial1.println("Initializing tusb");
-  tuh_init(0);
-  Serial1.println("HII, setup finished");
+  // Serial1.printf("xfer_cb buflen: %d daddr %d ep addr %d result %d user data %s\n", xfer->buflen, xfer->daddr, xfer->ep_addr, xfer->result, (char *)(xfer->user_data));
 }
 
-uint32_t count = 0;
-long long loop_time = 0;
-
-void loop()
+/*------------- MAIN -------------*/
+int count = 0;
+void setup1()
 {
-  status = WiFi.status();
-  if (status != WL_CONNECTED)
+  set_sys_clock_khz(120000, true);
+  // board_init();
+
+  Serial1.begin(115200);
+  Serial1.printf("TinyUSB Bare API Example\r\n");
+
+  sleep_ms(10);
+
+  pio_usb_configuration_t pio_cfg = PIO_USB_DEFAULT_CONFIG;
+  pio_cfg.pin_dp = 27;
+  //pio_cfg.alarm_pool = alarm_pool_create(1, 1);
+  //tuh_configure(1, TUH_CFGID_RPI_PIO_USB_CONFIGURATION, &pio_cfg);
+
+  // init host stack on configured roothub port
+  //tuh_init(1); // TODO, run this line in core 1
+
+  while (1)
   {
-    status = WiFi.begin(WIFISSID, WIFIPASS);
-    Serial1.printf("attempted reconnect %d %d %s %s %d\n", millis(), status, WIFISSID, WIFIPASS, tuh_inited());
+    // tinyusb host task
+    tuh_task();
+    cdc_task();
+
+    if (count % 1900000 == 0)
+    {
+      // Serial1.printf("%d 0, 1, 2, 3: %d %d %d %d %d\n", count, tuh_mounted(0), tuh_mounted(1), tuh_mounted(2), tuh_cdc_serial_is_mounted(1));
+      // Serial1.printf("%d %d %d, %d %d %d, %d \n", cdch_data[0].itf_num, cdch_data[0].ep_in, cdch_data[0].ep_out, cdch_data[1].itf_num, cdch_data[1].ep_in, cdch_data[1].ep_out);
+      //  tuh_cdc_receive(1, serial_in_buffer, sizeof(serial_in_buffer), true)); // waiting for next data
+    }
+    if (count % 1900000 == 0 && count / 1900000 == 3)
+      tuh_cdc_connect(1, tuh_cdc_xfer_cb);
+    count++;
+  }
+}
+void setup()
+{
+  int count1 = 0;
+  sleep_ms(2000);
+
+  Serial1.printf("Setting up wifi...0\n");
+  //already called by arduino stuffs, maybe double call is causing stucking????
+  //if (cyw43_arch_init())
+  //{
+  //  Serial1.printf("failed to initialize wifi\n");
+  //  return ;
+  //}
+
+  Serial1.printf("Setting up wifi...\n");
+  cyw43_arch_enable_sta_mode();
+  Serial1.printf("Setting up wifi...2\n");
+
+  // WiFi.setHostname("robo-pico");
+  int status = 1; // WiFi.begin(WIFISSID, WIFIPASS);
+  if (auto ans = cyw43_arch_wifi_connect_timeout_ms(WIFISSID, WIFIPASS, CYW43_AUTH_WPA2_AES_PSK, 30000); ans)
+  {
+    Serial1.printf("failed to connect. %d\n", ans);
+    return;
   }
   else
   {
-    if (count % 1000 == 0) // every 10 s
-    {
-      auto myip = WiFi.localIP();
-      Serial1.printf("--- Status: %d %s time: %d count: %d tuh_mounted: %d %d %d %d\n", status, myip.toString().c_str(), millis(), count, tuh_mounted(1), tuh_mounted(2), tuh_mounted(3), tuh_mounted(4));
-    }
-
-    if (!w.connected() && count % 100 == 0) // attempt every 100 ms
-      w.connect(SERVER_HOST, 10000);
-    if (count % 5000 == 0)
-    { // eevry 3000ms
-      w.write("HIIIIIIIII\n");
-      w.println(count);
-    }
-
-    if (w.available()) // read one set in one loop, if there's too much data, read in next loop to avoid stalling everything
-    {
-      Serial1.println(micros());
-      static char buf[1000];
-      int n = w.read(buf, 1000);
-      for (int i = min(100, n); i < 1000; i++)
-        buf[i] = 0;
-      Serial1.print(buf);
-      Serial1.flush();
-      Serial1.println(micros());
-    }
+    Serial1.printf("Connected. %d\n", ans);
   }
+  Serial1.printf("Wifi status %d\n", status);
 
-  do
+  while (1)
   {
-    tuh_task();
-  } while (loop_time + 1000 > micros());
-  // run loop every 1000 us
-
-  loop_time = micros();
-  count++;
+    cyw43_arch_poll();
+    if (count1 % (int)2e4 == 0 )//&& !client.connected())
+      Serial1.printf("retrying connect TCP status: %d\n", 0);//client.connect(HOST, 10001));
+    sleep_ms(1);
+    count1++;
+  }
 }
-
-CFG_TUSB_MEM_SECTION static char serial_in_buffer[64] = {0};
-
-void tuh_mount_cb(uint8_t daddr)
+void loop()
 {
-  Serial1.printf("Device attached, address = %d\n", daddr);
-  tuh_cdc_receive(daddr, serial_in_buffer, sizeof(serial_in_buffer), true); // schedule first transfer
-}
-
-void tuh_umount_cb(uint8_t daddr)
-{
-  Serial1.printf("Device removed, address = %d\n", daddr);
 }
 
 // invoked ISR context
 void tuh_cdc_xfer_isr(uint8_t dev_addr, xfer_result_t event, cdc_pipeid_t pipe_id, uint32_t xferred_bytes)
 {
-  Serial1.printf("xfer %d\n", dev_addr);
   (void)event;
   (void)pipe_id;
   (void)xferred_bytes;
 
-  Serial1.printf(serial_in_buffer);
+  // Serial1.printf("cdc_receive: addr: %d\n", dev_addr);
+  //  Serial1.printf("%s", serial_in_buffer);
+
+  // for (int i = 0; i < xferred_bytes; i++)
+  //   Serial1.printf("%c", serial_in_buffer[i]);
+  int status = -1; // client.write(serial_in_buffer, xferred_bytes);
+  Serial1.printf("BRAIN->COMPUTER TCP STATUS: %d len: %d\n", status, xferred_bytes);
+  //client.flush(); // TODO: timeout to not stall USB logic????
+
+  // Serial1.println();
+  // for (int i = 0; i < xferred_bytes; i++)
+  //  Serial1.printf("%02x", serial_in_buffer[i]);
+  //  Serial1.println();
+
   tu_memclr(serial_in_buffer, sizeof(serial_in_buffer));
 
-  tuh_cdc_receive(dev_addr, serial_in_buffer, sizeof(serial_in_buffer), true); // waiting for next data
+  // tuh_cdc_receive(dev_addr, serial_in_buffer, sizeof(serial_in_buffer), true); // waiting for next data
+}
+
+void cdc_task(void)
+{
+  // if (count % 10 == 0)
+  tuh_cdc_receive(1, serial_in_buffer, sizeof(serial_in_buffer), true);
+
+  // reading bytes 1 by 1 for now, but TODO read a bunch at once
+  int ch = Serial1.read(); // client.read();
+  if (ch >= 0)
+    tuh_cdc_send(1, &ch, 1, false);
+}
+
+/*------------- TinyUSB Callbacks -------------*/
+
+// Invoked when device is mounted (configured)
+void tuh_mount_cb(uint8_t daddr)
+{
+  Serial1.printf("Device attached, address = %d %d\r\n", daddr, cdch_data[daddr].ep_in);
+
+  // tuh_cdc_receive(daddr, serial_in_buffer, sizeof(serial_in_buffer), true)); // waiting for next data
+  // Get Device Descriptor
+  // TODO: invoking control transfer now has issue with mounting hub with multiple devices attached, fix later
+  // tuh_descriptor_get_device(daddr, &desc_device, 18, print_device_descriptor, 0);
+}
+
+/// Invoked when device is unmounted (bus reset/unplugged)
+void tuh_umount_cb(uint8_t daddr)
+{
+  Serial1.printf("Device removed, address = %d\r\n", daddr);
+}
+
+//
+int serial1_printf(const char *__restrict format, ...)
+{
+  char buf[256];
+  int len;
+  va_list ap;
+  va_start(ap, format);
+  len = vsnprintf(buf, sizeof(buf), format, ap);
+  Serial1.write(buf);
+  va_end(ap);
+  return len;
 }
